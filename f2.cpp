5c4fef37feea61f36c9be4e3e128761d58bc3e47
Yabin Cui <splintcoder@gmail.com>
2016-08-27 08:55:46 -0700

    Test pointer,array, struct.

--- f2.cpp
+++ f2.cpp
@@ -4,13 +4,31 @@ void f1();
 
 extern "C" {
 
-void __attribute__((visibility ("default"))) f2();
-int __attribute__((visibility ("default"))) incValue(int);
-double __attribute__((visibility ("default"))) incDoubleValue(double);
-void __attribute__((visibility ("default"))) incInlineValue(int*);
+#define EXPORT __attribute__((visibility("default")))
+
+void f2() EXPORT;
+int incValue(int) EXPORT;
+double incDoubleValue(double) EXPORT;
+void incInlineValue(int*) EXPORT;
+int* retPointer() EXPORT;
+void printString(const char*) EXPORT;
+const char* retString() EXPORT;
+void incArray(int* s, int n) EXPORT;
+int* retArray() EXPORT;
+
+struct MyStruct {
+	int count;
+	int* s;
+};
+
+void printStruct(struct MyStruct) EXPORT;
+void printStructP(struct MyStruct*) EXPORT;
+struct MyStruct retStruct() EXPORT;
+struct MyStruct* retStructP() EXPORT;
+
+void callCallback(int (*)()) EXPORT;
 }
 
-//void __attribute__((visibility ("default"))) f2() {
 void f2() {
   printf("this is f2\n");
   f1();
@@ -26,4 +44,74 @@ double incDoubleValue(double value) {
 
 void incInlineValue(int* value) {
   *value += 1;
+}
+
+int* retPointer() {
+	static int i = 0;
+	return &i;
+}
+
+void printString(const char* s) {
+	printf("%s\n", s);
+}
+
+const char* retString() {
+	return "Hello, world";
+}
+
+void incArray(int* s, int n) {
+	for (int i = 0; i < n; ++i) {
+		s[i] = i + 1;
+	}
+}
+
+int* retArray() {
+	static int s[10];
+	for (int i = 0; i < 10; ++i) {
+		s[i] = i + 1;
+	}
+	return s;
+}
+
+void printStruct(MyStruct myStru) {
+	for (int i = 0; i < myStru.count; ++i) {
+		printf("%d ", myStru.s[i]);
+	}
+	printf("\n");
+}
+
+void printStructP(MyStruct* p) {
+	for (int i = 0; i < p->count; ++i) {
+		printf("%d ", p->s[i]);
+	}
+	printf("\n");
+}
+
+MyStruct retStruct() {
+	MyStruct myStru;
+	myStru.count = 10;
+	static int s[10];
+	myStru.s = s;
+	for (int i = 0; i < 10; ++i) {
+		s[i] = i + 1;
+	}
+	return myStru;
+}
+
+MyStruct* retStructP() {
+	static MyStruct myStru;
+	myStru.count = 10;
+	static int s[10];
+	myStru.s = s;
+	for (int i = 0; i < 10; ++i) {
+		s[i] = i + 1;
+	}
+	return &myStru;
+}
+
+void callCallback(int (*callback)()) {
+	for (int i = 0; i < 10; ++i) {
+		printf("%d ", callback());
+	}
+	printf("\n");
 }
--- test.py
+++ test.py
@@ -2,16 +2,16 @@
 
 import ctypes as ct
 
-lib = ct.CDLL("libf2.so")
+lib = ct.CDLL("./libf2.so")
 
-# pass int
+# pass and return int
 a = ct.c_int(0)
 while a.value < 1000:
   a.value = lib.incValue(a)
 
 print a.value
 
-# pass double
+# pass and return double
 a = ct.c_double(0.0)
 incDoubleValue = lib.incDoubleValue
 incDoubleValue.restype = ct.c_double
@@ -21,5 +21,101 @@ while a.value < 999.8:
 print a.value
 
 # pass pointer as argument
-i = c_int(0)
-p = 
+i = ct.c_int(0)
+p = ct.pointer(i)
+while i.value < 1000:
+    lib.incInlineValue(p)
+
+print i.value
+
+# return pointer
+retPointerFunc = lib.retPointer
+retPointerFunc.restype = ct.POINTER(ct.c_int)
+for i in range(1000):
+    p = retPointerFunc()
+    p[0]=p[0]+1
+
+p = retPointerFunc()
+print p[0]
+
+# pass string as argument
+s = ct.create_string_buffer('\000' * 32)
+s.value = "hello, world"
+lib.printString(s)
+
+# return string
+retStringFunc = lib.retString
+retStringFunc.restype = ct.c_char_p
+print retStringFunc()
+
+# pass array as argument
+a = (ct.c_int * 10)()
+lib.incArray(a, 10)
+for i in range(10):
+    print a[i],
+print
+
+# return array pointer
+retArrayFunc = lib.retArray
+retArrayFunc.restype = ct.POINTER(ct.c_int)
+p = retArrayFunc()
+for i in range(10):
+    print p[i],
+print
+
+# pass struct as argument
+class MyStruct(ct.Structure):
+    _fields_ = [('count', ct.c_int),
+                 ('s', ct.POINTER(ct.c_int))]
+
+myStru = MyStruct()
+myStru.count = 10
+a = (ct.c_int * 10)()
+myStru.s = ct.cast(a, ct.POINTER(ct.c_int))
+for i in range(10):
+    myStru.s[i] = i + 1
+for i in range(10):
+    print myStru.s[i],
+print
+
+lib.printStruct(myStru)
+lib.printStructP(ct.pointer(myStru))
+
+# return struct
+retStructFunc = lib.retStruct
+retStructFunc.restype = MyStruct
+myStru = retStructFunc()
+for i in range(myStru.count):
+    print myStru.s[i],
+print
+
+retStructPFunc = lib.retStructP
+retStructPFunc.restype = ct.POINTER(MyStruct)
+p = retStructPFunc()
+for i in range(p[0].count):
+    print p[0].s[i],
+print
+
+
+# python callback function called from c
+CALLBACK_TYPE = ct.CFUNCTYPE(ct.c_int);
+
+a = 0
+def callback_func():
+    global a
+    a = a + 1
+    return a
+
+callback = CALLBACK_TYPE(callback_func)
+lib.callCallback(callback)
+
+
+
+
+
+
+
+
+
+
+
